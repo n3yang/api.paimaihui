e5b362ad087039920c3f4f0772a9adb1a9d1ae5f
n3yang <n3yang@gmail.com>
2012-09-18 00:54:08 +0800

    add paginator

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -13,6 +13,29 @@ class IndexController extends Zend_Controller_Action
         // action body
     }
 
-
+    /**
+     * 列表页
+     * 
+     */
+	public function listAction()
+	{
+		$data = array();
+		for ($i = 0; $i < 100; $i++) {
+			$data[$i] = $i . 'xxx';
+		}
+		$paginator = Zend_Paginator::factory(100);
+		$paginator->setDefaultItemCountPerPage(2);
+		$paginator->setCurrentPageNumber($this->_getParam('page'));
+		$this->view->paginator = $paginator;
+	}
+	
+	/**
+	 * 详情页
+	 * 
+	 */
+	public function showAction()
+	{
+		
+	}
 }
 
