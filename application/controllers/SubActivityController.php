bba48a27231e311baa153450645139e57823bf7a
n3yang <n3yang@gmail.com>
2012-12-13 18:50:08 +0800

    add: list of sub-activity

---
+++ application/controllers/SubActivityController.php
@@ -0,0 +1,55 @@
+<?php
+
+class SubActivityController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+    
+    
+	public function showAction()
+	{
+		$perpage = 20;
+		$pageno  = $this->getRequest()->getParam('page', 1);
+		$id = $this->getRequest()->getParam('id');
+		
+		if (!$id) {
+			throw new Zend_Controller_Exception();
+		}
+		// get sub-activity info
+		$tSubActivity = new Application_Model_DbTable_SubActivity();
+		$subActivity = $tSubActivity->find($id)->toArray();
+		$subActivity = $subActivity[0];
+		if (!$subActivity) {
+			throw new Zend_Controller_Exception();
+		} else {
+			// get antique
+			$tAntique = new Application_Model_DbTable_Antique();
+			$antiques = $tAntique->findBySubActivityId($id, $perpage, ($pageno-1)*$perpage);
+			$this->view->antiques = $antiques;
+			$total = $tAntique->countBySubAcitivityId($id);
+			// get activity
+			$tActivity = new Application_Model_DbTable_Activity();
+			$activity = $tActivity->find($subActivity['activity_id'])->toArray();
+			$subActivity['activity'] = $activity[0];
+			// get company
+			$tCompany = new Application_Model_DbTable_Company();
+			$company = $tCompany->find($activity[0]['company_id'])->toArray();
+			$subActivity['company'] = $company[0];
+			
+		}
+		$this->view->subActivity = $subActivity;
+		
+		$paginator = Zend_Paginator::factory(intval($total));
+		$paginator->setDefaultItemCountPerPage($perpage);
+		$paginator->setCurrentPageNumber($pageno);
+		$this->view->paginator = $paginator;
+	}
+}
--- application/models/DbTable/Antique.php
+++ application/models/DbTable/Antique.php
@@ -5,6 +5,53 @@ class Application_Model_DbTable_Antique extends Zend_Db_Table_Abstract
 
     protected $_name = 'antique';
 
-
+	/**
+	 * 根据subActivityId标记查询记录
+	 * 
+	 * @param int $subActivityId
+	 * @param int $count
+	 * @param int $offset
+	 * @return Zend_Db_Table_Rowset_Abstract
+	 */
+	public function findBySubActivityId($subActivityId, $count=20, $offset=0, $order='')
+	{
+		$where = $this->getAdapter()->quoteInto('sub_id=?', $subActivityId);
+		
+		$antiques = $this->fetchAll(
+			$this->select()
+				->from($this, '*')
+				->where($where)
+				->limit($count, $offset)
+				->order($order)
+			)->toArray();
+		
+		if (!empty($antiques)) {
+			// get photoes
+			foreach ($antiques as $v) {
+				$ids[] = $v['id'];
+			}
+			$photoTable = new Application_Model_DbTable_Photo();
+			$where = $photoTable->getAdapter()->quoteInto('antique_id in (?)', $ids);
+			$photoes = $photoTable->fetchAll($where)->toArray();
+			// format antique
+			foreach ($antiques as $k=>$v) {
+				foreach ($photoes as $pk=>$pv) {
+					if ($v['id'] == $pv['antique_id']) {
+						$antiques[$k]['photo'] = $pv;
+					}
+				}
+			}
+		}
+		
+		return $antiques;
+	}
+	
+	public function countBySubAcitivityId($subActivityId)
+	{
+		$where = $this->getAdapter()->quoteInto('sub_id=?', $subActivityId);
+		$rs = $this->fetchRow($this->select()->from($this, 'count(*) as total')->where($where))->toArray();
+		return $rs['total'];
+	}
+	
 }
 
