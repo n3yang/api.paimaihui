62567e67ce6b16343e36570e5daba8fb12567861
n3yang <n3yang@gmail.com>
2013-07-12 17:42:59 +0800

    udpate: friend link

--- application/models/DbTable/FriendLink.php
+++ application/models/DbTable/FriendLink.php
@@ -2,7 +2,9 @@
 
 class Application_Model_DbTable_FriendLink extends Zend_Db_Table_Abstract
 {
-
+	const IS_PUBLISHED_YES = 1;
+	const IS_PUBLISHED_NO = 0;
+	
     protected $_name = 'friend_link';
 
 
